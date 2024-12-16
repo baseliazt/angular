23fd8d5e08bf6d882cd02659693230692f8885cb
milyasbpa <basilyas.2015@gmail.com>
2024-12-16 20:02:25 +0700

    learn router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,8 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  // imports: [RouterOutlet],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'angular_frontend';
-}
+export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from './pages/products/products.component';
 import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
   {
     path: '',
+    component: AppComponent,
+  },
+  {
+    path: 'products',
     component: ProductsComponent,
   },
 ];
