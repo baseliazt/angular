6f6e8e07f25d239ec41ca21818f6c331960f7c7a
milyasbpa <basilyas.2015@gmail.com>
2024-12-16 20:21:06 +0700

    feat: add tailwind css into project

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
+  public title: WritableSignal<string> = signal('test');
 }
