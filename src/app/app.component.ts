fbec1167cc70565075d8f18c3d8d727788c679cc
milyasbpa <basilyas.2015@gmail.com>
2024-12-16 23:38:12 +0700

    fix: add header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApplicationComponent } from "./core/modules/application/container/application/application.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ApplicationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/core/modules/application/container/application/application.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavigationComponent } from "../../fragments/navigation/navigation.component";
+
+@Component({
+  selector: 'app-application',
+  imports: [NavigationComponent],
+  templateUrl: './application.component.html',
+  styleUrl: './application.component.scss'
+})
+export class ApplicationComponent {
+
+}
